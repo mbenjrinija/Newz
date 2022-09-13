0ae5a341ff8bc15070e4ee0058f4fce0d19dc6b7
Mouad <mouad.benjrinija@teal.ma>
2022-09-13 17:07:05 +0100

    create project

---
+++ Newz/System/NewzApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewzApp.swift
+//  Newz
+//
+//  Created by MOUAD BENJRINIJA on 13/9/2022.
+//
+
+import SwiftUI
+
+@main
+struct NewzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
